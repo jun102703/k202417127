0c45e9f353f209594ffdfd8ad4696955955d7afe
6202-42 <6202-42@6202-42>
2025-10-20 16:02:13 +0900

    four

--- src/git_test3a/Main.java
+++ src/git_test3a/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[]args) {
 		System.out.println("이재준");
 		System.out.println("안녕하세요");
+		World.run();
 	}
 
 }
---
+++ src/git_test3a/World.java
@@ -0,0 +1,7 @@
+package git_test3a;
+
+public class World {
+	public static void run() {
+        System.out.println("Hello, World!");
+    }
+}
